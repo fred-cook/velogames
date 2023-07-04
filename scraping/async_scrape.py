4951d7e53237d5cbba805cf6f3c19ab115b9f062
fred-cook <duverfred@yahoo.co.uk>
2023-07-04 19:55:51 +0100

    initial commit

---
+++ scraping/async_scrape.py
@@ -0,0 +1,16 @@
+from typing import Callable
+import asyncio
+
+import aio_http
+import pandas as pd
+
+
+def scraper(urls: list[str], ):
+    """
+
+    """
+
+async def process_url(url: str, parser: Callable) -> pd.DataFrame:
+    """
+    
+    """
